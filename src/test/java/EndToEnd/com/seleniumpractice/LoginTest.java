daa3f449377846b9bff379ee3009789fa50646f0
aryayogesh87 <136439330+aryayogesh87@users.noreply.github.com>
2025-05-11 08:26:03 +0530

    first commit

---
+++ src/test/java/EndToEnd/com/seleniumpractice/LoginTest.java
@@ -0,0 +1,12 @@
+package EndToEnd.com.seleniumpractice;
+
+import org.testng.annotations.Test;
+
+public class LoginTest {
+	
+	@Test
+	public void mytestone() {
+		System.out.println("this is my first test");
+	}
+
+}
