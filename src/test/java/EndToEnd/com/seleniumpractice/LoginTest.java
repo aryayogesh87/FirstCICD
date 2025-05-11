fa9db4ef66b9b8aac7c187078899a84c96f60ff4
aryayogesh87 <136439330+aryayogesh87@users.noreply.github.com>
2025-05-11 12:15:03 +0530

    adding extend report dependency

--- src/test/java/EndToEnd/com/seleniumpractice/LoginTest.java
+++ src/test/java/EndToEnd/com/seleniumpractice/LoginTest.java
@@ -1,12 +1,29 @@
 package EndToEnd.com.seleniumpractice;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 import org.testng.annotations.Test;
 
 public class LoginTest {
-	
-	@Test
-	public void mytestone() {
-		System.out.println("this is my first test");
-	}
 
+    @Test
+    public void mytestone() {
+        // Set report file path
+        ExtentSparkReporter spark = new ExtentSparkReporter("src/test/resources/ExtentReports/index.html");
+
+        // Create ExtentReports instance and attach reporter
+        ExtentReports extent = new ExtentReports();
+        extent.attachReporter(spark);
+
+        // Create a test in the report
+        ExtentTest test = extent.createTest("mytestone");
+        test.pass("This is my first test - PASSED");
+
+        // Print to console (optional)
+        System.out.println("this is my first test");
+
+        // Finalize the report
+        extent.flush();
+    }
 }
